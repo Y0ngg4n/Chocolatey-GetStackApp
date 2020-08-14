b864bad6c06babd7903d218a477d1ac483411de9
Y0ngg4n <andreschuele@gmail.com>
2020-08-14 15:40:00 +0200

    Fix pack

--- packVersion.ps1
+++ packVersion.ps1
@@ -7,30 +7,6 @@ Start-BitsTransfer -Source $checksumUrl -Destination "Stack.exe"
 
 $checksum   = (Get-FileHash -Path "Stack.exe" -Algorithm SHA256).Hash
 
-(Get-Content tools/chocolateyinstall.ps1) `
-    -replace '#REPLACE_VERSION#', $version |
-  Out-File tools/chocolateyinstall.ps1
+Write-Host $checksum
 
-(Get-Content tools/chocolateyinstall.ps1) `
-    -replace '#REPLACE_CHECKSUM#', $checksum `
-    -replace '#REPLACE_CHECKSUM_64#', $checksum |
-  Out-File tools/chocolateyinstall.ps1
-
-(Get-Content getstack-app.nuspec) `
-    -replace '#REPLACE_VERSION#', $version |
-Out-File getstack-app.nuspec
-
-choco pack
-
-(Get-Content tools/chocolateyinstall.ps1) `
-    -replace $version, '#REPLACE_VERSION#' |
-  Out-File tools/chocolateyinstall.ps1
-
-(Get-Content tools/chocolateyinstall.ps1) `
-    -replace $checksum, '#REPLACE_CHECKSUM#' `
-    -replace $checksum, '#REPLACE_CHECKSUM_64#' |
-  Out-File tools/chocolateyinstall.ps1
-
-(Get-Content getstack-app.nuspec) `
-    -replace $version, '#REPLACE_VERSION#' |
-Out-File getstack-app.nuspec
+Read-Host ‚Press Enter to continue…‘ | Out-Null
